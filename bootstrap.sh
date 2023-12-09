c2a73b463dfab5cd8f98a16cfd00dde3cc5a7f0e
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2023-12-09 15:01:52 +0100

    fix: install global dependencies before installing topics

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,24 @@ source "$DOTFILES_SCRIPTS_DIR/setup_topics.sh"
 
 cd $DOTFILES_ROOT_DIR
 
+install_dependencies() {
+	# install curl
+	execute "install_packages curl" "Installing curl ..."
+
+	# install rustup & cargo
+	execute "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh" "Installing rustup ..."
+
+	# install nvm & node
+	execute "PROFILE=/dev/null bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash'" "Installing nvm ..."
+}
+
 # ----------------------------------------------------------------------
 # | Main                                                               |
 # ----------------------------------------------------------------------
 
 main() {
+
+	install_dependencies
 
  	execute "setup_topics $DOTFILES_SOURCE_DIR"
 
