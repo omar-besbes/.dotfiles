54350483b51a9ec3bd8b331709d98b3c91b34b56
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2023-12-20 15:32:56 +0100

    chore: improve shell options

--- src/shell/core/options.sh
+++ src/shell/core/options.sh
@@ -3,6 +3,9 @@
 # Append to the history file, rather than overwriting it.
 shopt -s histappend
 
+# Save multi-line commands as one command.
+shopt -s cmdhist
+
 # Check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
@@ -22,11 +25,34 @@ shopt -s nocaseglob
 # match all files and zero or more directories and subdirectories.
 shopt -s globstar
 
+# Correct spelling errors in arguments supplied to cd
+shopt -s cdspell
+
+# Correct spelling errors during tab-completion
+shopt -s dirspell direxpand
+
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
 HISTCONTROL=ignoreboth
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+# Huge history. Doesn't appear to slow things down so why not ?
+HISTSIZE=500000
+HISTFILESIZE=100000
+
+# Don't record some commands
+export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+
+# Use standard ISO 8601 timestamp
+# %F equivalent to %Y-%m-%d
+# %T equivalent to %H:%M:%S (24-hours format)
+HISTTIMEFORMAT='%F %T '
+
+# This defines where cd looks for targets.
+# Add the directories you want to have fast access to, separated by colon.
+# Ex: CDPATH=".:~:~/Documents" will look for targets in the current working directory, in home and in the ~/Documents folder
+CDPATH="."
+
+# Automatically trim long paths in the prompt (requires Bash 4.x)
+PROMPT_DIRTRIM=2
 
