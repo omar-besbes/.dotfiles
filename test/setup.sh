b53491a0257be1e07ab16992cdab151a2ee955cf
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2023-12-24 02:18:50 +0100

    fix: check if script is running interactively or not

--- test/setup.sh
+++ test/setup.sh
@@ -7,10 +7,20 @@ declare DOCKERFILE_PATH="$DIR/Dockerfile"
 # Build docker image
 docker build -t dotfiles-test -f $DOCKERFILE_PATH .
 
-# Run docker 
-docker run -it \
-	--cap-add SYS_ADMIN \
-	--device /dev/fuse:/dev/fuse \
-	--security-opt apparmor:unconfined \
-	dotfiles-test
+# Check if the script is running interactively
+if [ -t 0 ]; then
+	# Run docker interactively
+	docker run -it \
+		--cap-add SYS_ADMIN \
+		--device /dev/fuse:/dev/fuse \
+		--security-opt apparmor:unconfined \
+		dotfiles-test
+else
+	# Run docker non-interactively
+	docker run \
+		--cap-add SYS_ADMIN \
+		--device /dev/fuse:/dev/fuse \
+		--security-opt apparmor:unconfined \
+		dotfiles-test
+fi
 
