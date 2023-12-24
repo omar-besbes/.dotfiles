18d2c4892ab1f60b47f5015504451354c4af6e82
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2023-12-24 19:01:52 +0100

    fix: tests

--- test/setup.sh
+++ test/setup.sh
@@ -3,6 +3,9 @@
 # Init
 declare DIR="$(dirname "${BASH_SOURCE[0]}")"
 declare DOCKERFILE_PATH="$DIR/Dockerfile"
+declare ROOT_DIR="$(realpath "$DIR/..")"
+
+cd "$ROOT_DIR" || exit 1
 
 # Build docker image
 docker build -t dotfiles-test -f $DOCKERFILE_PATH .
@@ -11,6 +14,7 @@ docker build -t dotfiles-test -f $DOCKERFILE_PATH .
 if [ -t 0 ]; then
 	# Run docker interactively
 	docker run -it \
+		--name dotfiles \
 		--cap-add SYS_ADMIN \
 		--device /dev/fuse:/dev/fuse \
 		--security-opt apparmor:unconfined \
@@ -18,6 +22,7 @@ if [ -t 0 ]; then
 else
 	# Run docker non-interactively
 	docker run \
+		--name dotfiles \
 		--cap-add SYS_ADMIN \
 		--device /dev/fuse:/dev/fuse \
 		--security-opt apparmor:unconfined \
