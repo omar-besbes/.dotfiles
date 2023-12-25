bc96546a122ac3ef218923ff7d942006c20c21f8
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2023-12-25 23:59:18 +0100

    test: add test for remotely created dotfiles as well

--- test/setup.sh
+++ test/setup.sh
@@ -1,31 +1,92 @@
 #!/bin/bash
 
-# Init
+# ----------------------------------------------------------------------
+# | Init                                                               |
+# ----------------------------------------------------------------------
+
 declare DIR="$(dirname "${BASH_SOURCE[0]}")"
-declare DOCKERFILE_PATH="$DIR/Dockerfile"
 declare ROOT_DIR="$(realpath "$DIR/..")"
+declare DOCKERFILE_LOCAL_PATH="$DIR/Dockerfile.local"
+declare DOCKERFILE_REMOTE_PATH="$DIR/Dockerfile.remote"
 
-cd "$ROOT_DIR" || exit 1
+# ----------------------------------------------------------------------
+# | Functions                                                          |
+# ----------------------------------------------------------------------
 
-# Build docker image
-docker build -t dotfiles-test -f $DOCKERFILE_PATH .
+show_help() {
 
-# Check if the script is running interactively
-if [ -t 0 ]; then
-	# Run docker interactively
-	docker run -it \
-		--name dotfiles \
-		--cap-add SYS_ADMIN \
-		--device /dev/fuse:/dev/fuse \
-		--security-opt apparmor:unconfined \
-		dotfiles-test
-else
-	# Run docker non-interactively
+	echo ""
+	echo "Usage: $0 [--local|--remote]"
+	echo ""
+	echo "Options:"
+	echo "   --local    Execute tests on a machine with locally crafted dotfiles"
+	echo "   --remote   Execute tests on a machine with dotfiles fetched remotely"
+	echo ""
+	exit 1
+
+}
+
+build_docker_image() {
+
+	local -r option=$1
+	local -r dockerfile_path=$2
+	local -r image_name="dotfiles-test$option"
+
+	docker build -t "$image_name" -f "$dockerfile_path" .
+
+}
+
+create_docker_container() {
+
+	local -r option=$1
+	local -r container_name="dotfiles$option"
+	local -r image_name="dotfiles-test$option"
+	
+	# Check if the script is running interactively
+	local -r is_interactive=$( [ -t 0 ] && echo '-it' )
+
+	# Create docker container according to the option / interactiveness
 	docker run \
-		--name dotfiles \
+		--name "$container_name" \
 		--cap-add SYS_ADMIN \
 		--device /dev/fuse:/dev/fuse \
 		--security-opt apparmor:unconfined \
-		dotfiles-test
-fi
+		"$is_interactive" \
+		"$image_name"
+
+}
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+	cd "$ROOT_DIR" || exit 1
+
+	# Check the number of arguments
+	if [ "$#" -ne 1 ]; then
+	    show_help
+	fi
+
+	# Parse the argument
+	local option=$1
+	case "$option" in
+		--local)
+			build_docker_image "$option" "$DOCKERFILE_LOCAL_PATH"
+			create_docker_container "$option"
+			;;
+		--remote)
+			build_docker_image "$option" "$DOCKERFILE_REMOTE_PATH"
+			create_docker_container "$option"
+			;;
+		*)
+			# Invalid argument
+			show_help
+			;;
+	esac
+
+}
+
+main "$@"
 
