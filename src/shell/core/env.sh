5892ac7bb987fe2276916c78891cc775cc725716
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-07-06 17:08:57 +0100

    fix: unnecessary nvm & golang related env

--- src/shell/core/env.sh
+++ src/shell/core/env.sh
@@ -11,15 +11,6 @@ fi
 # NVIDIA Drivers
 export PATH="/usr/local/cuda-12./bin${PATH:+:${PATH}}"
 
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # proto
 export PROTO_HOME="$HOME/.proto"
 export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH"
-
-# Rust package manager (cargo)
-source "$HOME/.cargo/env"
-
