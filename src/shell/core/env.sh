94df4f40a17db07992cd8eb507c02dcc591e6185
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2025-10-15 20:18:00 +0100

    chore: golang, nvm & auto-cd when command not recognized (#33)

--- src/shell/core/env.sh
+++ src/shell/core/env.sh
@@ -10,3 +10,11 @@ source "$HOME/.cargo/env"
 
 # deno
 source "$HOME/.deno/env"
+
+# go
+export PATH="$PATH:/usr/local/go/bin"
+
+# node
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
