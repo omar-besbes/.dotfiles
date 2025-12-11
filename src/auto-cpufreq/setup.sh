7e9cb725c7a797ffbf390d134eb662740d0e7bf1
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2025-12-11 18:37:13 +0100

    feat: auto-cpufreq

---
+++ src/auto-cpufreq/setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------
+# | Symlinks                                                           |
+# ----------------------------------------------------------------------
+
+create_symlinks() {
+
+  local -a FILES_TO_SYMLINK=("$TOPIC_DIR/auto-cpufreq.conf")
+  local -r TARGET_PATHS=("$HOME/.config/auto-cpufreq/auto-cpufreq.conf")
+  symlink_files FILES_TO_SYMLINK[@] TARGET_PATHS[@]
+
+}
+
+# ----------------------------------------------------------------------
+# | Dependencies                                                       |
+# ----------------------------------------------------------------------
+
+install_dependencies() {
+
+  # Optional, recommended to use with auto-cpufreq
+  sudo apt-get update
+  sudo apt-get install -y thermald 
+  
+  git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+  cd auto-cpufreq && sudo ./auto-cpufreq-installer
+  sudo auto-cpufreq --install
+  cd - && rm -rf auto-cpufreq
+
+}
+
+# ----------------------------------------------------------------------
+# | Main                                                               |
+# ----------------------------------------------------------------------
+
+main() {
+
+  local DIR="$(dirname "${BASH_SOURCE[0]}")"
+  local ROOT_DIR="$(realpath "$DIR/../..")"
+  local TOPIC_NAME="git"
+  local TOPIC_DIR="$ROOT_DIR/src/$TOPIC_NAME"
+
+  [ ! -v DOTFILES_ROOT_DIR ] && source "$ROOT_DIR/scripts/utils.sh"
+
+  ask_for_sudo
+  
+  install_dependencies
+
+  create_symlinks
+
+}
